82e21de26992040dc6337b4e2aca1704eeb5d00c
Abhishek S <127825992+abhishek8shankar@users.noreply.github.com>
2026-05-11 17:46:20 +0530

    Update postgres-init.sh
    Signed-off-by: Abhishek S <127825992+abhishek8shankar@users.noreply.github.com>

--- postgres-init/postgres-init.sh
+++ postgres-init/postgres-init.sh
@@ -20,9 +20,7 @@ find . -type f ! -path "./db_scripts/*" -exec rm -f {} \;
 
 echo "Extracted only db_scripts"
 
-cd db_scripts/$MOSIP_DB_NAME
-
-cd db_scripts/$DB_UNAME
+cd "db_scripts/$MOSIP_DB_NAME/$DB_UNAME"
 
 echo "Executing db_script"
 
